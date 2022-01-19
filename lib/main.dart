59d9216617784c615ec0becd76675f96cb6cea2d
im <imsunyong@naver.com>
2022-01-19 18:57:36 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:chatting_app/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginSignupScreen(),
+    );
+  }
+}
